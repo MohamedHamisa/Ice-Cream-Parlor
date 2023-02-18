62318c0c2393d7e1423217aca4903111b94594bf
Mohamed Hamisa <108889205+MohamedHamisa@users.noreply.github.com>
2023-02-18 23:44:12 +0200

    Create code.py

---
+++ code.py
@@ -0,0 +1,44 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'icecreamParlor' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER m
+#  2. INTEGER_ARRAY arr
+#
+
+def icecreamParlor(m, arr):
+    # Write your code here
+    a = []
+    for i in range(n):
+        for j in range(i+1,n):
+            if arr[i] + arr[j] ==m:
+                a.append(i+1)
+                a.append(j+1)
+                return a
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        m = int(input().strip())
+
+        n = int(input().strip())
+
+        arr = list(map(int, input().rstrip().split()))
+
+        result = icecreamParlor(m, arr)
+
+        fptr.write(' '.join(map(str, result)))
+        fptr.write('\n')
+
+    fptr.close()
